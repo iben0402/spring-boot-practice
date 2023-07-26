e6e21bbcc9244814fedb425b9829411ec3911c7d
iben0402 <81482747+iben0402@users.noreply.github.com>
2023-07-26 17:38:04 +0200

    code for custom component scanning

--- 02-spring-boot-spring-core/src/main/java/com/iwona/secondProject/SecondProjectApplication.java
+++ 02-spring-boot-spring-core/src/main/java/com/iwona/secondProject/SecondProjectApplication.java
@@ -3,11 +3,16 @@ package com.iwona.secondProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// CUSTOM SCANNING FOR PACKAGES
+//@SpringBootApplication(
+//        scanBasePackages = {"com.iwona.secondProject",
+//                "com.iwona.util"}
+//)
 @SpringBootApplication
 public class SecondProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecondProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecondProjectApplication.class, args);
+    }
 
 }
