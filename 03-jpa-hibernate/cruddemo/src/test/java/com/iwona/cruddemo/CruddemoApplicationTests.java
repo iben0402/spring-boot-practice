3814bf35ee158ab606b129b645c9cead566fc727
Iwona Bendig <81482747+iben0402@users.noreply.github.com>
2024-02-23 09:54:13 +0100

    added files for 3 part of course

---
+++ 03-jpa-hibernate/cruddemo/src/test/java/com/iwona/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iwona.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
